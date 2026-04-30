2a5d439128a274a5a6dcde8689a4538dbc3ab6b3
Shivam Kumar Dubey <sivamdby99@gmail.com>
2026-04-30 20:07:12 +0530

    Update data.js

--- next/app/pdf/data/data.js
+++ next/app/pdf/data/data.js
@@ -1,24 +1,24 @@
 export const page = [
   {
-    id: "1QG5nXp3sKdrTfRK5vVMHjeFIPM1FLGE8",
+    id: "1QG5nXp3sKdrTfRvVMHjeFIPM1FLGE8",
     title: "operating system notes",
     slug: "operating-system-notes",
     type: "notes",
   },
   {
-    id: "1Dy4cmSY-A7ZAm0pbdMFToxl-9gADUEPu",
+    id: "1Dy4cmSY-A7ZApbdMFToxl-9gADUEPu",
     title: "ada-handwritten-notes",
     slug: "ada-handwritten-notes",
     type: "notes",
   },
   {
-    id: "1PKtwNPQrGrKKZa22Gz-YHzsBEk4xn-fS",
+    id: "1PKtwNPQrGrKKZa22Gz-YsBEk4xn-fS",
     title: "c-language",
     slug: "c-language",
     type: "notes",
   },
    {
-    id: "1fbOu6JnUnSF9pCwRaH5GLFNuJfApejt-",
+    id: "1fbOu6JnUnSF9pCwRaH5GLFJfApejt-",
     title: "big data",
     slug: "big-data",
     type: "notes",
@@ -30,26 +30,26 @@ export const page = [
     type: "notes",
   },
    {
-    id: "1SCF8mhiDPauFTcsdDfd6Nkq8lYVyncmE",
+    id: "1SCF8mhiDPauFTcsdfd6Nkq8lYVyncmE",
     title: "data structures (DS)",
     slug: "ds",
     type: "notes",
   },
    {
-    id: "1E-L7KjsCdSc7eHz02F7LN-AZT0KzDo_c",
+    id: "1E-L7KjsCdSc7eHz02F7LN-AZT0Ko_c",
     title: "data mining and warehouse",
     slug: "data-mining-and-warehouse",
     type: "notes",
   },
    {
-    id: "1cYRykrW7NHakZtGjFcEHVRjhalbT_Naw",
+    id: "1cYkrW7NHakZtGjFcEHVRjhalbT_Naw",
     title: "software enginring",
     slug: "software-enginring",
     type: "notes",
   },
 
    {
-    id: "1ayHII0mvemiH0wkh-6vO2iT0EzwbAMl_",
+    id: "1aII0mvemiH0wkh-6vO2iT0EzwbAMl_",
     title: "software architecture",
     slug: "software-architecture",
     type: "notes",
@@ -68,31 +68,31 @@ export const page = [
   },
   // Interciew from here
   {
-    id: "1nFKlD5lGoV-y4oR14q-A6R2BMkZuaHpk",
+    id: "1nFKlD5lGoV-y4oR14q-A2BMkZuaHpk",
     title: "java",
     slug: "300-java-interview-questions",
     type: "interview",
   },
     {
-    id: "1MKBGKrESpEA2bQ5I-prd9QxDZSZX4_GL",
+    id: "1MKBGKrpEA2bQ5I-prd9QxDZSZX4_GL",
     title: "c language interview questions",
     slug: "220-c-language-interview-questions",
     type: "interview",
   },
     {
-    id: "1SgEJgzZDmd_XAoO_JKRec5OW537skyXI",
+    id: "1SgEJgzZDmd_XA_JKRec5OW537skyXI",
     title: "aws interview questions",
     slug: "aws-interview-questions",
     type: "interview",
   },
     {
-    id: "1VhShDKKoD-Q9AOTbuIc8BL5Ivm3MMfLw",
+    id: "1VhShDKKoD-Q9AOTbuIcL5Ivm3MMfLw",
     title: "microservices interview questions (130+ questions)",
     slug: "130-microservices-interview-questions",
     type: "interview",
   },
     {
-    id: "1-LrToVJZffjcdcetVkOv_gkN4n6KjlR-",
+    id: "1-LrToVJZffjcdcetVkOv_gkN6KjlR-",
     title: "docker interview questions",
     slug: "175-docker-interview-questions",
     type: "interview",
